57c406869df5fa8ddd4e09e28ef8817b67c54fef
Mathias Schmidt <mathias.schmidt1986@me.com>
2021-03-21 12:59:56 +0100

    Initiale commit with all necessary files

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# id3-from-beatport
-Simple script to grab some ID3 information from Beatport.
+# ID3 from Beatport
+
+Simple script to scrap some ID3 information from Beatport.
+
+I greated this PHP script after I found no solution to tag my songs with valid ID3 tags. I loved [Beatport Pro](https://www.beatport.com/mac/index.html) and the possibility to tag all tracks in my collection. But now, the program is no more available.
+
+### How it works?
+
+I take an ID of a Beatport track and parse the information direct from the website. With this information the script rename all track files and set the MP3 ID3 tags.
+
+### How to run?
+
+1. Clone or download (and extract) the script to your local machine from the [Github repository](https://github.com/mathiassname/id3-from-beatport)
+1. Open the terminal / command line and go to the directory
+1. Copy some MP3 files in the included *track/* folder
+1. Rename all MP3 files with the following schema: ```<Beatport ID>_<some name>.mp3``` (e.g. ```123456789_lorem ipsum.mp3```)
+1. Run the PHP script with the following command: ```php index.php```
+
+### Output file names
+
+The script rename all the input MP3 tracks. The schema of the renamig is:
+
+```<Beatport ID>_<Artist>_<Track title>_<Genre>.mp3```
+
+### License
+
+This script is published under the GNU General Public License v3 without any warranty.
+
+### How to support me?
+
+It exists many ways to support free software:
+
+* Report issues: https://github.com/mathiassname/id3-from-beatport/issues
+* Share *ID3 from Beatport* with your friends
+* Buy me a coffee ☕️: https://www.paypal.com/donate/?hosted_button_id=G5QWZVJSCTRKA 
---
+++ index.php
@@ -0,0 +1,406 @@
+<?php
+
+error_reporting(E_ALL);
+
+$url = 'https://www.beatport.com/track/-/';
+
+$baseDir = dirname(__FILE__).'/';
+$cacheDir = $baseDir.'cache/';
+$trackDir = $baseDir.'tracks/';
+
+echo "\n";
+echo 'Konfiguration'."\n";
+echo "\t".'Base directory: '.$baseDir."\n";
+echo "\t".'Cache directory: '.$cacheDir."\n";
+echo "\t".'Track directory: '.$trackDir."\n";
+echo "\n";
+
+/**
+ * Read track directory
+ * @param $trackDir
+ * @param $cacheDir
+ * @return array
+ */
+function readTracks($trackDir, $cacheDir)
+{
+    $tracks = [];
+
+    if ($hdl = opendir($trackDir)) {
+        while ($file = readdir($hdl)) {
+            $parts = pathinfo($file);
+            if ($parts['extension'] === 'mp3') {
+                preg_match('/^([0-9]{7,9})_(.*)\.mp3$/', $parts['basename'], $matches);
+
+                $track = [
+                    'id' => $matches[1] ?? null,
+                    'filename' => $file,
+                    'filepath' => $trackDir.$file,
+                    'cachepath' => null,
+                ];
+
+                if (!empty($track['id'])) {
+                    $track['cachepath'] = $cacheDir.$track['id'].'.html';
+                    $tracks[$track['id']] = $track;
+                }
+            }
+        }
+
+        closedir($hdl);
+    }
+
+    return $tracks;
+}
+
+/**
+ * Get track information from beatport
+ * @param $url
+ * @return mixed|null
+ */
+function getTrack($url)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 3);
+    curl_setopt($ch, CURLOPT_URL, $url);
+
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    if (!$result) {
+        return null;
+    }
+
+    return $result;
+}
+
+/**
+ * Grab image and save them
+ * @param $url
+ * @param $savePath
+ * @return bool|null
+ */
+function grabImage($url, $savePath)
+{
+    if (file_exists($savePath)) {
+        return false;
+    }
+
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_BINARYTRANSFER, 1);
+
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    if (!$result) {
+        return null;
+    }
+
+    if ($fp = fopen($savePath, 'x')) {
+        fwrite($fp, $result);
+        fclose($fp);
+
+        return true;
+    }
+
+    return null;
+}
+
+/**
+ * Generate new file names from track information
+ * @param $track
+ * @return string
+ */
+function generateNewFileName($track)
+{
+    $newFilename =
+        $track['id'].'_'.
+        $track['information']['artist'].'_'.
+        $track['information']['full_title'].'_'.
+        $track['information']['genre'].
+        '.mp3';
+
+    $newFilename = str_replace(['/', '\\'], ' ', $newFilename);
+    $newFilename = normalize($newFilename);
+
+    return $newFilename;
+}
+
+/**
+ * Normalize text
+ * @param $text
+ * @return string
+ */
+function normalize($text)
+{
+    $text = strip_tags($text);
+    $text = trim($text);
+    $text = preg_replace('/\s+/', ' ', $text);
+
+    return $text;
+}
+
+// read tracks from directory
+$tracks = readTracks($trackDir, $cacheDir);
+
+if (!empty($tracks) && count($tracks) > 0) {
+    // load beatport pages & information
+    echo 'Load beatport pages'."\n";
+    foreach ($tracks as $track) {
+        echo "\t".$track['filename'].' -> '.$url.$track['id'];
+
+        if (!file_exists($track['cachepath'])) {
+            $html = getTrack($url.$track['id']);
+
+            if (!empty($html)) {
+                file_put_contents($track['cachepath'], $html);
+            }
+
+            echo " -> downloaded";
+        } else {
+            echo " -> already exists";
+        }
+
+        echo "\n";
+    }
+
+    echo "\n";
+
+    // now, it is time to extract the track information
+    echo 'Extract beatport information'."\n";
+
+    libxml_use_internal_errors(true);
+
+    foreach ($tracks as &$track) {
+        echo "\t".$track['id'].': ';
+
+        $track['information'] = [];
+
+        // load file
+        $dom = new DomDocument;
+        $dom->loadHTMLFile($track['cachepath']);
+
+        // create xpath
+        $xpath = new DomXPath($dom);
+
+        // title & remix
+        $nodes = $xpath->query("//div[@class='interior-title']/h1");
+        foreach ($nodes as $i => $node) {
+            if ($i === 0) {
+                $track['information']['title'] = normalize($node->nodeValue);
+                echo '*';
+            } elseif ($i === 1) {
+                $track['information']['remix'] = normalize($node->nodeValue);
+                echo '*';
+            }
+        }
+
+        // full title
+        if (!empty($track['information']['title']) && !empty($track['information']['remix'])) {
+            $track['information']['full_title'] = $track['information']['title'].' ('.$track['information']['remix'].')';
+            echo '*';
+        }
+
+        // artist
+        $nodes = $xpath->query("//div[@class='interior-track-artists']/span[@class='value']");
+        foreach ($nodes as $i => $node) {
+            if ($i === 0) {
+                $track['information']['artist'] = normalize($node->nodeValue);
+                echo '*';
+            } elseif ($i === 1) {
+                $track['information']['remixer'] = normalize($node->nodeValue);
+                echo '*';
+            }
+        }
+
+        // album
+        $nodes = $xpath->query("//li[@class='interior-track-releases-artwork-container ec-item']/@data-ec-name");
+        foreach ($nodes as $i => $node) {
+            $track['information']['album'] = normalize($node->nodeValue);
+            echo '*';
+        }
+
+        // genre
+        $nodes = $xpath->query("//li[@class='interior-track-content-item interior-track-genre']/span[@class='value']");
+        foreach ($nodes as $i => $node) {
+            $track['information']['genre'] = normalize($node->nodeValue);
+            echo '*';
+        }
+
+        // label
+        $nodes = $xpath->query("//li[@class='interior-track-content-item interior-track-labels']/span[@class='value']");
+        foreach ($nodes as $i => $node) {
+            $track['information']['label'] = normalize($node->nodeValue);
+            echo '*';
+        }
+
+        // cover
+        $nodes = $xpath->query("//img[@class='interior-track-release-artwork']/@src");
+        foreach ($nodes as $i => $node) {
+            $track['information']['cover'] = normalize($node->nodeValue);
+            echo '*';
+        }
+
+        // release date
+        $nodes = $xpath->query("//li[@class='interior-track-content-item interior-track-released']/span[@class='value']");
+        foreach ($nodes as $i => $node) {
+            $track['information']['release_date'] = normalize($node->nodeValue);
+            echo '*';
+        }
+
+        // bpm
+        $nodes = $xpath->query("//li[@class='interior-track-content-item interior-track-bpm']/span[@class='value']");
+        foreach ($nodes as $i => $node) {
+            $track['information']['bpm'] = normalize($node->nodeValue);
+            echo '*';
+        }
+
+        // key
+        $nodes = $xpath->query("//li[@class='interior-track-content-item interior-track-key']/span[@class='value']");
+        foreach ($nodes as $i => $node) {
+            $track['information']['key'] = normalize($node->nodeValue);
+            echo '*';
+        }
+
+        echo "\n";
+    }
+
+    echo "\n";
+
+    // ok, download the images
+    echo 'Grab track covers'."\n";
+
+    foreach ($tracks as &$track) {
+        echo "\t".$track['filename'];
+
+        if (isset($track['information']['cover']) && !empty($track['information']['cover'])) {
+            echo ' -> '.$track['information']['cover'];
+
+            $parts = pathinfo($track['information']['cover']);
+            $coverPath = $cacheDir.$parts['basename'];
+
+            $status = grabImage($track['information']['cover'], $coverPath);
+
+            if ($status === true) {
+                $track['information']['cover'] = $coverPath;
+
+                echo ' -> image grabbed';
+            } elseif ($status === false) {
+                echo '  -> already exists';
+            } else {
+                echo '  -> image not grabbed and saved';
+            }
+        } else {
+            echo '  -> no cover information';
+        }
+
+        echo "\n";
+    }
+
+    echo "\n";
+
+    // it is time to rename the files
+    echo 'Rename files'."\n";
+
+    foreach ($tracks as &$track) {
+        echo "\t".$track['filename'];
+
+        $newFilename = generateNewFileName($track);
+
+        $track['filename_new'] = $newFilename;
+        $track['filepath_new'] = $trackDir.$newFilename;
+
+        echo ' -> '.$track['filename_new'];
+
+        if (!file_exists($track['filepath_new'])) {
+            if (rename($track['filepath'], $track['filepath_new'])) {
+                echo ' -> renamed';
+            } else {
+                echo ' -> renaming went wrong';
+            }
+        } else {
+            echo ' -> file not renamed';
+        }
+
+        echo "\n";
+    }
+
+    // all is in our information bag, now it is time to write id3 tags
+    echo 'Tag files'."\n";
+
+    require_once 'vendor/getID3/getid3/getid3.php';
+
+    foreach ($tracks as &$track) {
+        echo "\t".$track['filename_new'];
+
+        $getID3 = new getID3;
+        $getID3->setOption(array('encoding' => 'UTF-8'));
+
+        getid3_lib::IncludeDependency(GETID3_INCLUDEPATH.'write.php', __FILE__, true);
+
+        $data = [
+            'track_number' => [$track['id']],
+            'artist' => [$track['information']['artist'] ?? ''],
+            'title' => [$track['information']['full_title'] ?? ''],
+            'remixer' => [$track['information']['remixer'] ?? ''],
+            'album' => [$track['information']['album'] ?? ''],
+            'genre' => [$track['information']['genre'] ?? ''],
+            'publisher' => [$track['information']['label'] ?? ''],
+            'year' => [substr($track['information']['release_date'] ?? date('Y-m-d'), 0, 4)],
+            'original_release_time' => [$track['information']['release_date'] ?? ''],
+            'release_time' => [$track['information']['release_date'] ?? ''],
+            'popularimeter' => array('email' => '', 'rating' => 255, 'data' => 0),
+            'bpm' => [(int)$track['information']['bpm'] ?? ''],
+            'initial_key' => [$track['information']['key'] ?? ''],
+        ];
+
+        if ($img = file_get_contents($track['information']['cover'])) {
+            if ($exif = exif_imagetype($track['information']['cover'])) {
+                $parts = pathinfo($track['information']['cover']);
+                $data['attached_picture'] = [
+                    [
+                        'data' => $img,
+                        'picturetypeid' => '03',
+                        'description' => $parts['filename'],
+                        'mime' => image_type_to_mime_type($exif),
+                    ],
+                ];
+
+                echo ' -> Cover added';
+            } else {
+                echo ' -> Invalid image format (only GIF, JPEG, PNG)';
+            }
+        } else {
+            echo ' -> Can not open '.$track['information']['cover'];
+        }
+
+        $tagwriter = new getid3_writetags;
+        $tagwriter->filename = $track['filepath_new'];
+        $tagwriter->tagformats = array('id3v1', 'id3v2.4');
+        $tagwriter->tag_encoding = 'UTF-8';
+        $tagwriter->overwrite_tags = true;
+        $tagwriter->remove_other_tags = true;
+        $tagwriter->tag_data = $data;
+
+        if ($tagwriter->WriteTags() === true) {
+            echo ' -> File tagged';
+        } else {
+            echo ' -> No tags written'."\n";
+            echo "\n";
+            var_dump($tagwriter->errors);
+            echo "\n";
+        }
+
+        echo "\n";
+    }
+
+    echo "\n";
+} else {
+    echo 'No tracks available.'."\n";
+    echo "\n";
+}
+
+echo 'DONE!'."\n";
+echo "\n";
